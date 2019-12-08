7e733be4a497418019bdc5797d10898b3476e75d
julia.marsal <julia.mp2209@gmail.com>
2019-12-08 19:17:45 +0100

    point features code uploaded

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # webcam_point_features
-Detection of ORB features from online webcam imges.
+see in  [ORB](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_orb/py_orb.html)
+
+## ORB Definition:
+###ORB: Oriented Fast and Rotated BRIEF
+
+ORB is a feature detection and description, basically it's a fusion between a corner detection [FAST](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_fast/py_fast.html) and [BRIEF](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_brief/py_brief.html() descriptor with many modifications  to enhance the performance.
+First it use FAST algorithm to find keypoints, then it applies Harris corner measure to find top N points among them and it also use pyramid to produce multiscale-features.
+As FAST doesn't compute the orientation, the algorithm computes the intensity weighted centroid of the patch with located corner at center. The direction of the vector from this corner point to centroid gives the orientation and computes moments with x and y. Then, BRIEF descriptors are used according to the orientation of keypoints.
+
+For descriptor matching, multi-probe [LSH](https://en.wikipedia.org/wiki/Locality-sensitive_hashing) is used.
+
+The paper says ORB is much faster than SURF and SIFT and ORB descriptor works better than SURF. ORB is a good choice in low-power devices for panorama stitching etc.
+
+## CODE
+
+There are the following functions:
+
+1. orb_base(input image, keypoints to compute, detector to compute, descriptor to compute) -> is the base ORB feature detection function.
+2. orb_base_mask(input image, keypoints to compute,detector to compute, descriptor to compute, mask rectangle x,mask rectangle y, mask rectangle width, mask rectangle height) -> is the base ORB feature detection function computed to a mask.
+3. FastFeatureDetector(input image, keypoints to compute) -> keypoint detector based on FAST corner detection.
+4. BriskDetector(input image, keypoints to compute, descriptor to compute) -> keypoint detector based on Brisk and description extractor
+
+### Output
+
+It obtains the following images:
+1. Keypoint detector algorithms: original image, FAST obtained image, BRISK obtained image
+2. ORB detection descriptor: original image, ORB feature detector, ORB feature detector with mask.
+3. As it is shown in the following image:
+
+![picture](Image.png)
+
+
+## Run the code
+
+1. Make a new file named build
+2. go to build and execute *cmake ..* and then *make*.
--- src/point_features.cpp
+++ src/point_features.cpp
@@ -12,15 +12,55 @@
 //consts
 const unsigned int MIN_NUM_FEATURES = 300; //minimum number of point fetaures
 
+void orb_base(cv::Mat img, std::vector<cv::KeyPoint> keypoints,  cv::Ptr<cv::ORB> detector,cv::Mat descriptor){
+  //clear previous points
+  keypoints.clear();
+  //detect and compute(extract) features
+  //keypoints, descriptors	=	cv.Feature2D.detectAndCompute(	image, mask[, descriptors[, useProvidedKeypoints]]	)
+  detector->detectAndCompute(img, cv::noArray(), keypoints, descriptor);
+  //draw points on the image
+  //cv::drawKeypoints (InputArray image, const std::vector< KeyPoint > &keypoints, InputOutputArray outImage, const Scalar &color=Scalar::all(-1), int flags=DrawMatchesFlags::DEFAULT)
+  cv::drawKeypoints(img, keypoints, img, 255, cv::DrawMatchesFlags::DEFAULT );
+
+}
+
+void orb_base_mask(cv::Mat img, std::vector<cv::KeyPoint> keypoints,  cv::Ptr<cv::ORB> detector, cv::Mat descriptor,int x, int y, int width, int height){
+  cv::Mat mask = cv::Mat::zeros(img.size(), CV_8UC1);  // type of mask is CV_8U
+  cv::Rect rect(x, y, width, height); //defining the rectangle where roi will be placed
+  cv::Mat roi(mask, rect); //defining region of interest
+  cv::rectangle(img, rect, cv::Scalar(0, 255, 0));  //rectangle(imageORBmask, 10, 10, 255, int thickness=1, int lineType=8, int shift=0)¶ //drawing green rectangle
+  roi = cv::Scalar(255);  // roi is a sub-image of mask specified by cv::Rect object // we set elements in roi region of the mask to 255
+  detector->detectAndCompute(img,mask,keypoints, descriptor);
+  cv::drawKeypoints(img,keypoints,img,255,cv::DrawMatchesFlags::DEFAULT );
+}
+
+//**************keypoint Detectors ***************/
+
+void FastFeatureDetector(cv::Mat img, std::vector<cv::KeyPoint> keypoints){
+      cv::Ptr<cv::FastFeatureDetector> Fast = cv::FastFeatureDetector::create();
+      Fast->detect(img, keypoints); //detect keypoints
+      cv::drawKeypoints(img,keypoints,img,255,cv::DrawMatchesFlags::DEFAULT );
+
+}
+
+  void BriskDetector(cv::Mat img,std::vector<cv::KeyPoint> keypoints, cv::Mat descriptor ){
+      cv::Ptr<cv::BRISK> BRISK = cv::BRISK::create();
+      //BRISK->detect(img, keypoints);
+      BRISK->detectAndCompute(img,cv::noArray(),keypoints,descriptor);
+      cv::drawKeypoints(img,keypoints,img,255,cv::DrawMatchesFlags::DEFAULT );
+
+  }
+
 int main(int argc, char *argv[])
 {
     cv::VideoCapture camera; //OpenCV video capture object
     cv::Mat image; //OpenCV image object
-	int cam_id; //camera id . Associated to device number in /dev/videoX
+	  int cam_id; //camera id . Associated to device number in /dev/videoX
     cv::Ptr<cv::ORB> orb_detector = cv::ORB::create(); //ORB point feature detector
     orb_detector->setMaxFeatures(MIN_NUM_FEATURES);
     std::vector<cv::KeyPoint> point_set; //set of point features
     cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor
+
 
 	//check user args
 	switch(argc)
@@ -57,23 +97,48 @@ int main(int argc, char *argv[])
             cv::waitKey();
         }
 
+    //****define more images***//
+    cv::Mat imageORB;
+    image.copyTo(imageORB);
+
+    cv::Mat imageORBmask;
+    image.copyTo(imageORBmask);
+
+    cv::Mat imageFFD;
+    image.copyTo(imageFFD);
+
+    cv::Mat imageSBD;
+    image.copyTo(imageSBD);
+
+
     //**************** Find ORB point fetaures and descriptors ****************************
 
-        //clear previous points
-        point_set.clear();
+    orb_base(imageORB, point_set,  orb_detector,descriptor_set);
 
-        //detect and compute(extract) features
-        orb_detector->detectAndCompute(image, cv::noArray(), point_set, descriptor_set);
+    //**************** ##Try to detect features more sparse over all the image, by using the mask:****************************
 
-        //draw points on the image
-        cv::drawKeypoints( image, point_set, image, 255, cv::DrawMatchesFlags::DEFAULT );
+    orb_base_mask(imageORBmask, point_set, orb_detector, descriptor_set,10, 10, 400, 400);
 
-    //********************************************************************
+    //**************** ##Fast feature detector of keypoints:****************************
 
-        //show image
-        cv::imshow("Output Window", image);
+    FastFeatureDetector(imageFFD, point_set);
+
+    //**************** ##SimpleBriskDetector:****************************
+
+    BriskDetector(imageSBD, point_set,descriptor_set);
+
+    //**************** ##SimpleBlobDetector:****************************
+
+    cv::Mat matArray[3] = {image,imageORB,imageORBmask};
+    cv::hconcat(matArray,3,imageORB);
+    cv::Mat mat2[3] = {image,imageFFD,imageSBD};
+    cv::hconcat(mat2,3,imageFFD);
+    cv::imshow("keypoints detector algoritms: original image, FAST, BRISK", imageFFD);
+    cv::imshow("Original image, ORB feature detector, ORB featre detector with mask", imageORB);
+
+
 
 		//Waits 30 millisecond to check if 'q' key has been pressed. If so, breaks the loop. Otherwise continues.
     	if( (unsigned char)(cv::waitKey(30) & 0xff) == 'q' ) break;
-    }   
+    }
 }
